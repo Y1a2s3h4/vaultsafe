b8acf8aa64202d40883691361a0e6f4a3594d11d
Y1a2s3h4 <yashpurohit763@gmail.com>
2023-07-09 13:39:17 +0530

    feat: added lamba func addData endpoint

---
+++ functions/addData/addData.ts
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv'
+import { Handler, HandlerEvent } from '@netlify/functions'
+import VaultSafeModel from '../vaultSafeModel';
+import mongoose, {ConnectOptions} from 'mongoose';
+export const handler: Handler = async (event: HandlerEvent) => {
+  try {
+    dotenv.config({
+      path: "functions\\.env",
+    });
+    if (event.body === null) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Invalid request body' })
+      };
+    }
+    const MongoUri: string = process.env.MONGO_URI || '';
+    mongoose
+      .connect(MongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions)
+      .then(() => console.log("Connected to MongoDB"))
+      .catch((err) => console.log(err));
+      
+    const body = JSON.parse(event.body);
+    const {
+      urlName,
+      pswd,
+      tabsList
+    } = body;
+    const vaultSafeObject = await VaultSafeModel.create({
+      urlName,
+      pswd,
+      tabsList
+    });
+    vaultSafeObject.save();
+    return { statusCode: 200, body: JSON.stringify(vaultSafeObject) };
+  } catch (error) {
+    console.log(error)
+    return { statusCode: 500, body: JSON.stringify(error) };
+  }
+}
